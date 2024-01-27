45a169f0bbe7bad0281a135922b61d98aab8b1e6
himanshu-joshi01 <himanshujoshi3110@gmail.com>
2024-01-28 03:51:07 +0530

    add max of 3 numbers

---
+++ JavaBasics/MaxOf3Num.java
@@ -0,0 +1,20 @@
+package JavaBasics;
+
+import java.util.Scanner;
+
+public class MaxOf3Num {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int a,b,c,max;
+        System.out.println("Enter the first number:");
+        a=sc.nextInt();
+        System.out.println("Enter the second number:");
+        b=sc.nextInt();
+        System.out.println("Enter the third number:");
+        c=sc.nextInt();
+            max = (a > b) ? ((a > c) ? a : c) : ((b > c) ? b : c);
+        System.out.println("The maximum value is "+max);
+
+
+    }   
+}
