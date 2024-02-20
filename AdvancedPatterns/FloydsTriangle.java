200b4503b892202c898175d3b0488b5d7c29b582
himanshu-joshi01 <himanshujoshi3110@gmail.com>
2024-02-21 02:17:31 +0530

    added Floyd pattern

---
+++ AdvancedPatterns/FloydsTriangle.java
@@ -0,0 +1,18 @@
+package AdvancedPatterns;
+import java.util.*;
+public class FloydsTriangle {
+    public static void main(String[] args) {
+    Scanner sc=new Scanner(System.in);
+    System.out.print("Enter No. of lines of triangle:");
+    int n=sc.nextInt();
+    int count=1;
+
+    for(int i=1;i<=n;i++){
+     for(int j=1;j<=i;j++){
+        System.out.print(count+" ");
+        count++;
+     }
+     System.out.println();
+    }
+    }
+    }
