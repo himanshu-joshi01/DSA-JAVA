e9a8e0bd71b3bc0b871ce9269a3ef915a6793e17
himanshu-joshi01 <himanshujoshi3110@gmail.com>
2024-02-21 02:18:36 +0530

    ude

--- AdvancedPatterns/FloydsTriangle.java
+++ AdvancedPatterns/FloydsTriangle.java
@@ -1,18 +1,20 @@
 package AdvancedPatterns;
+
 import java.util.*;
+
 public class FloydsTriangle {
     public static void main(String[] args) {
-    Scanner sc=new Scanner(System.in);
-    System.out.print("Enter No. of lines of triangle:");
-    int n=sc.nextInt();
-    int count=1;
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter No. of lines of triangle:");
+        int n = sc.nextInt();
+        int count = 1;
 
-    for(int i=1;i<=n;i++){
-     for(int j=1;j<=i;j++){
-        System.out.print(count+" ");
-        count++;
-     }
-     System.out.println();
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print(count + " ");
+                count++;
+            }
+            System.out.println();
+        }
     }
-    }
-    }
+}
