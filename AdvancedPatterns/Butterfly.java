fd98699b9818882131417d727ad14347e1c31445
himanshu-joshi01 <himanshujoshi3110@gmail.com>
2024-02-22 01:10:43 +0530

    updated

---
+++ AdvancedPatterns/Butterfly.java
@@ -0,0 +1,12 @@
+package AdvancedPatterns;
+import java.util.*;
+public class Butterfly {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+     System.out.println("Enter the number of lines in pattern:");
+     int n=sc.nextInt();
+
+     
+    }
+    
+}
--- AdvancedPatterns/MirroredRightTriangle.java
+++ AdvancedPatterns/MirroredRightTriangle.java
@@ -1,21 +1,22 @@
 package AdvancedPatterns;
+
 import java.util.*;
 
 public class MirroredRightTriangle {
     public static void main(String[] args) {
-                Scanner sc = new Scanner(System.in);
+        Scanner sc = new Scanner(System.in);
         System.out.println("Enter the no. of the lines of triangle:");
-        int n= sc.nextInt();
-        
-        for(int i=1;i<=n;i++){
-            for(int j=1;j<=n-i;j++){
+        int n = sc.nextInt();
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n - i; j++) {
                 System.out.print(" ");
             }
-            for(int k=1;k<=i;k++){
+            for (int k = 1; k <= i; k++) {
                 System.out.print("*");
 
             }
-                System.out.println();
+            System.out.println();
 
         }
     }
