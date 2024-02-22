def3c6bd32847f52c5ec1e9807b5981b986090f5
himanshu-joshi01 <himanshujoshi3110@gmail.com>
2024-02-22 11:21:13 +0530

    Hollow rhombus pattern

---
+++ AdvancedPatterns/HollowRhombus.java
@@ -0,0 +1,26 @@
+import java.util.*;
+
+public class HollowRhombus {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the number of lines of Rhombus: ");
+        int n = sc.nextInt();
+        sc.close();
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n - i; j++) {
+                System.out.print("  ");
+            }
+
+            for (int k = 1; k <= n; k++) {
+                if (i == 1 || i == n || k == 1 || k == n) {
+                    System.out.print("* ");
+                } else {
+                    System.out.print("  ");
+                }
+            }
+            System.out.println();
+
+        }
+    }
+}
