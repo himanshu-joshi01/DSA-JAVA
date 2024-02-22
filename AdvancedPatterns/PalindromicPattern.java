b43587ec4cca69e5eaf7ba76dbe3bc04ca8b0d0d
himanshu-joshi01 <himanshujoshi3110@gmail.com>
2024-02-22 12:29:29 +0530

    Pallindromicpattern added

---
+++ AdvancedPatterns/PalindromicPattern.java
@@ -0,0 +1,24 @@
+import java.util.*;
+
+public class PalindromicPattern {
+        public static void main(String[] args) {
+            Scanner sc=new Scanner(System.in);
+            System.out.println("Enter the No. of lines in pattern:");
+            int n =sc.nextInt();
+
+                for (int i = 1; i <=n ; i++) {
+                    for(int j=1;j<=n-i;j++){
+                            System.out.print(" ");
+                    }
+                    for(int k=i;k>=1;k--){
+                        System.out.print(k);
+
+                    }
+                    for(int l=2;l<=i;l++){
+                            System.out.print(l);
+                    }
+                    System.out.println();
+                }
+
+                }
+        }
