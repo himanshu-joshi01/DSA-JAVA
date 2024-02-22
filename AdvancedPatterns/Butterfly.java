1c4312b893a2eb3c4127d94b9d51aec6a87a6b23
himanshu-joshi01 <himanshujoshi3110@gmail.com>
2024-02-22 10:48:15 +0530

    updated

--- AdvancedPatterns/Butterfly.java
+++ AdvancedPatterns/Butterfly.java
@@ -18,12 +18,12 @@ public class Butterfly {
                 System.out.print(" *");
             }
             System.out.println();
-
+            System.out.println();
         }
         for (int i = n; i >= 1; i--) {
             for (int j = 1; j <= i; j++) {
                 System.out.print("* ");
-            }
+            } 
             for (int k = 1; k <= 2 * (n - i); k++) {
                 System.out.print("  ");
             }
