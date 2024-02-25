e392a174736322e1c77c9f2c37622ef313f8faa4
himanshu-joshi01 <himanshujoshi3110@gmail.com>
2024-02-25 23:04:53 +0530

    added new file

--- AdvancedPatterns/PalindromicPattern.java
+++ AdvancedPatterns/PalindromicPattern.java
@@ -19,7 +19,7 @@ public class PalindromicPattern {
                 System.out.print(l);
             }
             System.out.println();
-            System.out.println();
+           
 
         }
 
