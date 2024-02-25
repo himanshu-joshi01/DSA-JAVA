6085aad5378b043d655e0bb23fc6f45428dd35e4
himanshu-joshi01 <himanshujoshi3110@gmail.com>
2024-02-25 23:01:54 +0530

    added new file

---
+++ JavaBasics/ReverseOfNumber.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+
+public class ReverseOfNumber {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a number:");
+        int num = sc.nextInt();
+        int rev=0;
+        sc.close();
+
+        while(num > 0) {
+            int rem=num%10;
+            rev=rev*10+rem;
+            num/=10;
+            }
+        
+        System.out.println("Reversed Number is : "+rev);
+    }
+    
+}
