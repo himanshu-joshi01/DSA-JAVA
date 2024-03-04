b7823a8e2a9db385ba2e1cfd70e8e04b9fe9c59f
himanshu-joshi01 <himanshujoshi3110@gmail.com>
2024-03-04 22:37:20 +0530

    added new file

---
+++ Arrays/MaxSumOfSubArrays/PrefixSum.java
@@ -0,0 +1,42 @@
+package MaxSumOfSubArrays;
+import java.util.*;
+
+public class PrefixSum {
+public static void MaxSubArraySum(int arr[]){
+    int n=arr.length;
+    int prefix[]=new int[n];
+    prefix[0]=arr[0];
+
+    //Calculate prefix array
+    for(int i=1; i<n;i++){
+        prefix[i]=prefix[i-1]+arr[i];
+    }
+
+    int currSum=0;
+    int maxSum= Integer.MIN_VALUE;
+
+    for(int i=0 ; i<n ; i++){
+        int start=i;
+        for(int j=i;j<n;j++){
+            int end=j;
+            currSum=start==0?prefix[end]:prefix 
+        }
+    }
+    
+    // Calculate the prefix sum array
+    
+
+}   
+
+public static void main(String args[]){
+    Scanner sc=new Scanner(System.in);
+    System.out.println("Enter No. of Elements in Array:");
+    int n=sc.nextInt();
+    int arr[]=new int[n];
+    System.out.println("Enter elements of array:");
+    for(int i=0 ; i<n ; i++){
+        arr[i]=sc.nextInt();
+    }
+    MaxSubArraySum(arr);
+}
+}
