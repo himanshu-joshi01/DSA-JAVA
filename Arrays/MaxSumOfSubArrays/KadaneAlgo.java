0c116cd5c223291cd553f91b90573d5663e0d657
himanshu-joshi01 <himanshujoshi3110@gmail.com>
2024-03-07 18:02:35 +0530

    modified kadane algorithm

--- Arrays/MaxSumOfSubArrays/KadaneAlgo.java
+++ Arrays/MaxSumOfSubArrays/KadaneAlgo.java
@@ -3,15 +3,17 @@ import java.util.*;
 public class KadaneAlgo {
  public static void maxSumSubArray(int arr[]){
     int n=arr.length;
-    int currSum=arr[0];
+    int currSum=0;
     int maxSum=Integer.MIN_VALUE;
-    for(int i=1 ; i<n ; i++)
+    for(int i=0; i<n ; i++)
     {
         currSum+=arr[i];
-        if(currSum<0){
-        currSum=0;
-        }
+        
         maxSum= Math.max(currSum,maxSum);
+
+        if(currSum<0){
+            currSum=0;
+            }
     }
 System.out.println("Max Sum of SubArray: "+maxSum);
  }
