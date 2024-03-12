f89c2a821b7a0568dc30ee36a17032eb7d742ede
himanshu-joshi01 <himanshujoshi3110@gmail.com>
2024-03-12 22:45:02 +0530

    add new file

--- Arrays/Searching/BinarySearch.java
+++ Arrays/Searching/BinarySearch.java
@@ -1,7 +1,8 @@
 import java.util.Scanner;
 import java.util.Arrays;
-public class BinarySearch{  
-    public static void binarySearch(int arr[], int first, int last, int key){  
+
+public class BinarySearch {
+   public static void binarySearch(int arr[], int first, int last, int key){  
       int mid = (first + last)/2;  
       while( first <= last ){  
          if ( arr[mid] < key ){  
@@ -15,30 +16,32 @@ public class BinarySearch{
          mid = (first + last)/2;  
       }  
       if ( first > last ){  
-         System.out.println("Element is not found!");  
+         System.out.println("Element is not found!");  J
       }  
-    }  
-    public static void main(String args[]){ 
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Enter the number of elements in array:");
-        int n = sc.nextInt();
-    
-        int[] arr = new int[n]; 
-            System.out.println("Enter elements of array ") ;
-            for(int i=0;i<n;i++){
-                arr[i]=sc.nextInt();
-            }     
-            Arrays.sort(arr);
-            System.out.println("Sorted Array:") ;
-            for(int i=0;i<n;i++){
-             System.out.print(arr[i]+" ") ;
-            }   System.out.println("") ;  
-            
-        System.out.print("Enter a search element : ");
-        int key = sc.nextInt() ;
-        sc.close(); 
-           int last=arr.length-1; 
-           int first=0 ;
-           binarySearch(arr,first,last,key);     
-    }  
-   }  
+    }
+
+   public static void main(String args[]) {
+
+      Scanner sc = new Scanner(System.in);
+      System.out.println("Enter the number of elements in array:");
+      int n = sc.nextInt();
+      int[] arr = new int[n];
+      System.out.println("Enter elements of array ");
+      for (int i = 0; i < n; i++) {
+         arr[i] = sc.nextInt();
+      }
+      Arrays.sort(arr);
+      System.out.println("Sorted Array:");
+      for (int i = 0; i < n; i++) {
+         System.out.print(arr[i] + " ");
+      }
+      System.out.println("");
+
+      System.out.print("Enter a search element : ");
+      int key = sc.nextInt();
+      sc.close();
+      int last = arr.length - 1;
+      int first = 0;
+      binarySearch(arr, first, last, key);
+   }
+}
---
+++ Arrays/Sorting/InsertionSort.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+public class InsertionSort {
+    {
+
+    }
+
+      public static void print(int arr[]) {
+        int n = arr.length;
+        System.out.println("The sorted array is: ");
+
+        for (int i = 0; i < n; i++) {
+            System.out.print(arr[i] + " ");
+        }
+    }
+
+
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the number of elements in array:");
+        int n = sc.nextInt();
+        int arr[] = new int[n];
+        System.out.println("Enter the elements of arrays");
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        sc.close();
+        sort(arr);
+        print(arr);
+    }
+
+}
+    
+}
