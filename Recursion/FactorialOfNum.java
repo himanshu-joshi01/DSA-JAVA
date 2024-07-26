e31ffc73e9d52271b2a4ace308aa673208bf97fb
HIMANSHU_JOSHI <127012017+himanshu-joshi01@users.noreply.github.com>
2024-07-26 20:41:48 +0530

    Factorial of number

---
+++ Recursion/FactorialOfNum.java
@@ -0,0 +1,21 @@
+
+import java.util.*;
+
+public class FactorialOfNum {
+
+    public static int fact(int n) {
+        if (n == 0 || n == 1) {
+            return 1;
+        }
+        int fact = n * fact(n - 1);
+        return fact;
+    }
+
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int n = sc.nextInt();
+        System.out.println("Factorial of " + n + " : " + fact(n));
+
+    }
+}
