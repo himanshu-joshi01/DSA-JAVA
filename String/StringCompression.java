c8896e77be0cd403e6cef3d97f6b052a2e230dba
HIMANSHU_JOSHI <127012017+himanshu-joshi01@users.noreply.github.com>
2024-08-31 16:14:44 +0530

    String compression

--- String/StringCompression.java
+++ String/StringCompression.java
@@ -1,24 +1,23 @@
 public class StringCompression {
     public static void main(String[] args){
         String str = "aaabbccccdddde";
-        
         System.out.println(stringCompression(str));
        
    }
    public static String stringCompression(String str){
-    String str1= "";
-    for(int i=0 ; i<str.length(); i++){
-        Integer count=1;
+    String str1 = "";
+    for(int i=0 ; i < str.length() ; i++){
+        Integer count = 1;
         while(i < str.length()-1 && str.charAt(i) == str.charAt(i+1) ){
-            count++;
+            count++; 
              i++;
         }
         str1 =str1 + str.charAt(i);
         if(count>1){
             str1 =str1 + count.toString();
-         }
-       }
+        }
+    }
         return str1;
-   }
+  }
    
 }
